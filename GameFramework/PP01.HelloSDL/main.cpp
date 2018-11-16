619b658e02942beeaee08e5bc700afacef31e56b
20171282 <20171282@vision.hoseo.edu>
2018-11-16 11:27:07 +0900

    PP02.BreakUpHellowSDL

--- GameFramework/PP01.HelloSDL/main.cpp
+++ GameFramework/PP01.HelloSDL/main.cpp
@@ -1,30 +1,55 @@
 #include<SDL.h>
 
+void init(){}
+void render(){}
+void update(){}
+void handleEvents(){}
+void clean(){}
+
+bool g_bRunning = true;
+
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 
-int main(int argc, char* args[])
+int main()
 {
+	init();
+	while (g_bRunning)
+	{
+		handleEvents();
+		update();
+		render();
+	}
+	clean();
+}
 
+bool init(const char* title, int xpos, int ypos,
+	int width, int height, int flags)
+{
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
+		g_pWindow = SDL_CreateWindow(title, xpos, ypos,
+			width, height, flags);
 
-		g_pWindow = SDL_CreateWindow("PP01.HelloSDL",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED,
-			640, 480, SDL_WINDOW_SHOWN);
-		if (g_pWindow != 0) {
+		if (g_pWindow != 0)
+		{
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
 		}
-	}
-	else {
-		return 1;
-	}
 
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
-	SDL_Delay(5000);
-	SDL_Quit();
-	return 0;
+	}
+	else
+	{
+		return false;
+	}
+	return true;
 }
+
+void render()
+{
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+
+	SDL_RenderClear(g_pRenderer);
+
+	SDL_RenderPresent(g_pRenderer);
+}
+
